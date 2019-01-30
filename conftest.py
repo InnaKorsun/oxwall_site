c0dc606e3ba06f06affb18f2834ef8528cfd58de
InnaKorsun <slvjova.inna@gmail.com>
2019-01-30 12:59:05 +0200

    add conftest

--- conftest.py
+++ conftest.py
@@ -2,12 +2,12 @@ import json
 import pytest
 import os.path
 from selenium import webdriver
-from oxwall_site_model import OxwallSite
+
 #import OxwallSite
 from value_models.user import User
 from db.db_connector import DBConnector
 from value_models.status import Status
-
+from oxwall_site_model import OxwallSite
 
 @pytest.fixture(scope='session')
 def driver():

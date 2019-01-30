c6ef096095051413a54144fbc8814fb45106bb20
InnaKorsun <slvjova.inna@gmail.com>
2019-01-30 12:49:01 +0200

    Add conftest again

--- conftest.py
+++ conftest.py
@@ -2,14 +2,14 @@ import json
 import pytest
 import os.path
 from selenium import webdriver
-from oxwall_site_model import OxwallSite
+# from oxwall_site_model import OxwallSite
+import OxwallSite
 from value_models.user import User
 from db.db_connector import DBConnector
 from value_models.status import Status
 
 
-
-@pytest.fixture(scope = 'session')
+@pytest.fixture(scope='session')
 def driver():
     # Open browser driver settings
     driver = webdriver.Chrome()
@@ -21,11 +21,11 @@ def driver():
     driver.quit()
 
 
-
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 with open(os.path.join(PROJECT_DIR, "config.json")) as f:
     config = json.load(f)
+
 
 @pytest.fixture(scope="session")
 def db():
@@ -33,15 +33,18 @@ def db():
     yield db
     db.close()
 
+
 with open(os.path.join(PROJECT_DIR, "data", "user_data.json")) as f:
     user_data = json.load(f)
 
-@pytest.fixture(params=user_data, ids=[str(user) for user in user_data],scope='session')
+
+@pytest.fixture(params=user_data, ids=[str(user) for user in user_data], scope='session')
 def user(request, db):
     user = User(**request.param)
     db.create_user(user)
     yield user
     db.delete_user(user)
+
 
 @pytest.fixture(scope="session")
 def admin():
@@ -54,12 +57,13 @@ def oxwall_app(driver):
     app = OxwallSite(driver)
     return app
 
+
 @pytest.fixture(scope="session")
 def signed_in_user(driver, user, oxwall_app):
-
     oxwall_app.login_as(user)
     yield user
     oxwall_app.logout_as(user)
+
 
 @pytest.fixture(scope="session")
 def signed_as_admin(driver, admin, oxwall_app):
@@ -67,9 +71,9 @@ def signed_as_admin(driver, admin, oxwall_app):
     yield admin
     oxwall_app.logout_as(admin)
 
-@pytest.fixture(scope="session")
-def status_for_test(driver,signed_in_user,oxwall_app):
 
+@pytest.fixture(scope="session")
+def status_for_test(driver, signed_in_user, oxwall_app):
     status = Status(text="Status for test", user=signed_in_user)
 
     oxwall_app.dash_page.status_text_field.input(status.text)
@@ -78,7 +82,7 @@ def status_for_test(driver,signed_in_user,oxwall_app):
 
 
 @pytest.fixture()
-def logout(driver,oxwall_app):
+def logout(driver, oxwall_app):
     yield
-    #app = OxwallSite(driver)
+    # app = OxwallSite(driver)
     oxwall_app.dash_page.sign_out()

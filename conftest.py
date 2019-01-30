75f8ed4bc91bdb33ff5dff84b7e290e53691fd7a
InnaKorsun <43093445+InnaKorsun@users.noreply.github.com>
2019-01-30 19:57:20 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,7 +3,7 @@ import pytest
 import os.path
 from selenium import webdriver
 
-from .value_models.user import User
+from value_models.user import User
 from .db.db_connector import DBConnector
 from value_models.status import Status
 from .oxwall_site_model import OxwallSite
